c651aed39d6ee8f5cd014d9b7b009e58c134b61c
TimBits <tim_bits_4_u@hotmail.com>
2012-11-08 18:03:55 -0800

    sum added

--- src/Bill.java
+++ src/Bill.java
@@ -4,10 +4,19 @@ import java.util.List;
 
 public class Bill {
 
+	private double total = 0;
+	private int count = 0;
 	private List<MenuItem> myList = new ArrayList<MenuItem>();
 	Menu m = new Menu();
 
 	public void addItem(int iNum){
 		myList.add(m.menu[iNum]);
+		count++;
+	}
+
+	public void sum(){
+		for(int i=0;i<count;i++){
+			total += myList.get(count).getPrice();
+		}
 	}
 }
