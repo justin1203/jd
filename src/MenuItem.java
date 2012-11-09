9067178ffb12782ec50e6b61aa41611d83b9fc64
Justin\ Lee <lhk890419@gmail.com>
2012-11-08 23:41:26 -0800

    minor edit 5

--- src/MenuItem.java
+++ src/MenuItem.java
@@ -1,18 +1,17 @@
-
 public class MenuItem {
-	
+
 	private String name;
 	private double price;
-	
+
 	public MenuItem(String n, double p){
 		name = n;
 		price = p;
 	}
-	
+
 	public double getPrice(){
 		return price;
 	}
-	
+
 	public String getName(){
 		return name;
 	}
