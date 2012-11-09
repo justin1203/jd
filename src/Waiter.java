1cf9e4194c54f0ce498240a6102838ab640be2d8
unknown <tigerphysics@gmail.com>
2012-11-08 16:15:00 -0800

    Testing..testing..

--- src/Waiter.java
+++ src/Waiter.java
@@ -1,10 +1,26 @@
 
 public class Waiter {
 
-	public void createWaiters(){
-		String[] waiter = new String[2];
-
-		waiter[0] = "Jon";
-		waiter[1] = "Jane";
+	String name;
+	double tips;
+	
+	public void createWaiters(String n){//The user provides the number
+		name = n;
+		tips = 0;//Initially the waiter has 0 tips
+	}
+	
+	//Get the waiter's name
+	public String getName(){
+		return name;
+	}
+	
+	//When a customer adds a tip, increment the total amount of tips
+	public void addTip(double amount){
+		tips += amount;
+	}
+	
+	//Return the sum of the waiter's tips
+	public double getTips(){
+		return tips;
 	}
 }
--- src/restaurant.java
+++ src/restaurant.java
@@ -6,6 +6,8 @@ public class restaurant {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Waiter waiter = new Waiter();
 
 	}
 
